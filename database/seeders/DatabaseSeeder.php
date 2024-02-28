d369744aa9f7c3565755dd7e5b63af2593f870e1
HakimRhouni <rhouni.hakim@gmail.com>
2024-02-28 19:29:06 +0100

    ajout Spaties et creation des roles admin superadmin et  user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-
+use App\Models\User;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,13 +15,32 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        DB::table('users')->insert([
+{
+    // Créer les rôles s'ils n'existent pas déjà
+    if (Role::where('name', 'superadmin')->doesntExist()) {
+        Role::create(['name' => 'superadmin']);
+    }
+
+    if (Role::where('name', 'admin')->doesntExist()) {
+        Role::create(['name' => 'admin']);
+    }
+
+    if (Role::where('name', 'user')->doesntExist()) {
+        Role::create(['name' => 'user']);
+    }
+
+    // Vérifier si l'utilisateur "admin" existe déjà
+    $existingAdmin = User::where('email', 'admin@argon.com')->first();
+
+    // Si l'utilisateur "admin" n'existe pas, créer un nouvel utilisateur
+    if (!$existingAdmin) {
+        User::create([
             'username' => 'admin',
             'firstname' => 'Admin',
             'lastname' => 'Admin',
-            'email' => 'admin@argon.com',
+            'email' => 'admin@argon.com', // Changer l'adresse e-mail si nécessaire
             'password' => bcrypt('secret')
-        ]);
+        ])->assignRole('superadmin');
     }
+}
 }
