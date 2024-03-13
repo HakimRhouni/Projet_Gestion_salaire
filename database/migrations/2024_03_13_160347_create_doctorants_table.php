2b8ef6f9cda2b2cedb04f8f42b9b5b325ef509ce
HakimRhouni <rhouni.hakim@gmail.com>
2024-03-13 16:25:42 +0000

    model et table migration doctorant

--- database/migrations/2024_03_13_160347_create_doctorants_table.php
+++ database/migrations/2024_03_13_160347_create_doctorants_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStagiairesTable extends Migration
+class CreateDoctorantsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,20 +13,14 @@ class CreateStagiairesTable extends Migration
      */
     public function up()
     {
-        Schema::create('stagiaires', function (Blueprint $table) {
+        Schema::create('doctorants', function (Blueprint $table) {
             $table->id();
             $table->string('nom');
             $table->string('prenom');
             $table->string('adresse');
-            $table->string('cin');
+            $table->string('numero_cin');
             $table->string('carte_sejour');
-            $table->string('numero_cnss');
-            $table->string('id_fiscal');
-            $table->decimal('montant_brut');
-            $table->decimal('indemnite');
-            $table->decimal('retenues');
-            $table->decimal('net_imposable');
-            $table->unsignedBigInteger('periode');
+            $table->decimal('brut_indemnites', 10, 2); // Montant brut des indemnités, décimal avec 10 chiffres au total, dont 2 décimales
             $table->unsignedBigInteger('id_societe');
             $table->foreign('id_societe')->references('id')->on('entreprises')->onDelete('cascade');
             $table->unsignedBigInteger('id_periode');
@@ -42,6 +36,6 @@ class CreateStagiairesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('stagiaires');
+        Schema::dropIfExists('doctorants');
     }
 }
